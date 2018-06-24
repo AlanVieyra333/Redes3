725b15ee87c389f75c7bd94c9045f687d35f317f
AlanVieyra333 <alanvieyra376@gmail.com>
2018-06-24 19:15:08 +0000

    Create install.sh

---
+++ config/observium/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+apt-get install libapache2-mod-php7.0 php7.0-cli php7.0-mysql php7.0-mysqli php7.0-gd php7.0-mcrypt \
+php7.0-json php-pear snmp fping mariadb-server mariadb-client python-mysqldb rrdtool subversion whois \
+mtr-tiny ipmitool graphviz imagemagick apache2
